41cbabf8024a529f6e9f721710046b1e71627277
Valdemar <pizza27@live.dk>
2024-05-27 13:05:18 +0200

    Update signup.js

--- javascript/signup.js
+++ javascript/signup.js
@@ -1,26 +1,29 @@
+const text = ["nametext","mobiltext"];
+const error = ["errorName","errorPhone"];
 let errorCount = 0;
 
 function confirmation() {
+    
     errorCount = 0;
-    if(document.getElementById("nametext").value==""){
-        errorCount++;
-        console.log(document.getElementById("errorName").style.display="block");
-    } else{
-        console.log(document.getElementById("errorName").style.display="none");
-    }
 
-    if(document.getElementById("mobiltext").value==""){
-        errorCount++;
-        console.log(document.getElementById("errorPhone").style.display="block");
-    } else{
-        console.log(document.getElementById("errorPhone").style.display="none");
+    for (let i = 0; i < text.length; i++) 
+    {
+        if(document.getElementById(text[i]).value=="")
+        {
+            errorCount++;
+            console.log(document.getElementById(error[i]).style.display="block");
+        }else
+        {
+            console.log(document.getElementById(error[i]).style.display="none");
+        }
     }
 
     if(errorCount == 0){
         document.getElementById("succesbox").style.display="block";
-        document.getElementById("nametext").disabled = true;
-        document.getElementById("emailtext").disabled = true;
-        document.getElementById("mobiltext").disabled = true;
-        document.getElementById("firmatext").disabled = true;
+        
+        const ids = ["nametext","emailtext","mobiltext","firmatext"];
+        for (let i = 0; i < ids.length; i++) {
+            document.getElementById(ids[i]).disabled = true;
         }
+    }
 }
