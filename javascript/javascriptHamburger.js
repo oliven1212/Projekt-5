f3a9536eb7b74d8b1633fec837d546d828472f76
Valdemar <pizza27@live.dk>
2024-05-28 10:51:37 +0200

    MIKKEL!

--- javascript/javascriptHamburger.js
+++ javascript/javascriptHamburger.js
@@ -1,13 +1,17 @@
+const names = ["Forside","Tilmeld","Aktiviteter","Kontakt"];
+const links = ["index","signup","activities","contact"];
+
+for (let i = 0; i <names.length; i++){
+    document.getElementById("myLinks").innerHTML += '<a href="'+links[i]+'.html"><p>'+names[i]+'</p></a>';
+}
 function burgerShow() {
-    for (let i = 0; i < 1; i++) {
-        let x = document.getElementById("myLinks");
-        let hamburger = document.querySelector('.hamburger');
-        if (x.style.display === "block") {
-            x.style.display = "none";
-            hamburger.classList.remove('show-menu');
-        } else {
-            x.style.display = "block";
-            hamburger.classList.add('show-menu');
-        }
+    let x = document.getElementById("myLinks");
+    let hamburger = document.querySelector('.hamburger');
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        hamburger.classList.remove('show-menu');
+    } else {
+        x.style.display = "block";
+        hamburger.classList.add('show-menu');
     }
 }
